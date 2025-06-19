9652e966360b10377dfd2b7c04573bae612d6657
Jorge Luis Arce <jorgearcedev1@gmail.com>
2025-06-18 19:47:58 -0700

    Creando state para actividades

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,52 +1,63 @@
+import { useState } from "react";
 import { categories } from "../data/categories";
 
 export default function Form() {
-  return (
-    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
-        <div className="grid grid-cols-1 gap-3">
-              <label htmlFor="category" className="font-bold">
-                Categoria:
-              </label>
-              <select
-                className="border border-slate-300 p-2 rounded-lg w-full bg-white"
-                id="category"
-              >
-                {categories.map((category) => (
-                  <option key={category.id} value={category.id}>
-                    {category.name}
-                  </option>
-                ))}
-              </select>
-        </div>
-      <div className="grid grid-cols-1 gap-3">
-            <label htmlFor="activity" className="font-bold">
-              Actividad:
-            </label>
-            <input
-              id="activity"
-              type="text"
-              className="border border-slate-300 p-2 rounded-lg"
-              placeholder="Ej. Comida, jugo de Naranja, ensalada, Ejercicio, pesas, bicicleta"
-            />
-      </div>
-      
-      <div className="grid grid-cols-1 gap-3">
-            <label htmlFor="calories" className="font-bold">
-              Calorias:
-            </label>
-            <input
-              id="calories"
-              type="text"
-              className="border border-slate-300 p-2 rounded-lg"
-              placeholder="Calorias, Ej: 400"
-            />
-      </div>
+  const[activity, setActivity] = useState({
+    category: 1,
+    name: "",
+    calories: 0
+  })
 
-      <input
-        type="submit"
-        className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
-       value={"Guardar comida o Guardar ejercicio"}
-       />         
-    </form>
-  );
+
+    return (
+        <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+            <div className="grid grid-cols-1 gap-3">
+                  <label htmlFor="category" className="font-bold">
+                    Categoria:
+                  </label>
+                  <select
+                    className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+                    id="category"
+                    value={activity.category}
+                  >
+                    {categories.map((category) => (
+                      <option key={category.id} value={category.id}>
+                        {category.name}
+                      </option>
+                    ))}
+                  </select>
+            </div>
+          <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="name" className="font-bold">
+                  Actividad:
+                </label>
+                <input
+                  id="name"
+                  type="text"
+                  className="border border-slate-300 p-2 rounded-lg"
+                  placeholder="Ej. Comida, jugo de Naranja, ensalada, Ejercicio, pesas, bicicleta"
+                  value={activity.name}
+                />
+          </div>
+          
+          <div className="grid grid-cols-1 gap-3">
+                <label htmlFor="calories" className="font-bold">
+                  Calorias:
+                </label>
+                <input
+                  id="calories"
+                  type="text"
+                  className="border border-slate-300 p-2 rounded-lg"
+                  placeholder="Calorias, Ej: 400"
+                  value={activity.calories}
+                />
+          </div>
+
+          <input
+            type="submit"
+            className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer"
+          value={"Guardar comida o Guardar ejercicio"}
+          />         
+        </form>
+    );
 }
