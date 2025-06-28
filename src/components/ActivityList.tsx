0bbb326966187626d222d3b66456c64ea95a90cb
Jorge Luis Arce <jorgearcedev1@gmail.com>
2025-06-27 18:43:50 -0700

    Añadiendo informacion al listado e icono para editar

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -1,4 +1,7 @@
 import type { Activity } from "../types";
+import { categories } from "../data/categories";
+import { useMemo } from "react";
+import {PencilSquareIcon} from '@heroicons/react/24/outline';
 
 type ActivityListProps = {
     activities: Activity[];
@@ -6,6 +9,9 @@ type ActivityListProps = {
 
 export default function ActivityList({ activities }: ActivityListProps) {
 
+  const categoryName = useMemo(() => 
+    (category : Activity['category']) => categories.map(cat => cat.id === category ? cat.name : '')
+  , [activities] )
     
   return <>
     <h2 className="text-4xl font-bold text-slate-600 text-center">
@@ -16,9 +22,10 @@ export default function ActivityList({ activities }: ActivityListProps) {
 
             <div key={activity.id} className="px-5 py-10 bg-white mt-5 flex justify-between">
                 <div className="space-y-2 relative">
-                  <p>
-                    {activity.category}
+                  <p className={`absolute -top-8 -left-8 px-10 py-2 text-white uppercase font-bold ${activity.category === 1 ? 'bg-lime-500' : 'bg-orange-500'}`}>
+                      {categoryName(+activity.category)}
                   </p>
+
                     <p className="text-2xl font-bold pt-5">{activity.name}</p>
                     <p className="font-black text-4xl text-lime-500">
                         {activity.calories} {''}
@@ -29,7 +36,12 @@ export default function ActivityList({ activities }: ActivityListProps) {
 
                 </div>
 
-                <div>
+                <div className="flex gap-5 items-center">
+                  <button>
+                     <PencilSquareIcon
+                        className="h-8 w-8 text-gray-800"
+                        />
+                  </button>
 
 
                 </div>
