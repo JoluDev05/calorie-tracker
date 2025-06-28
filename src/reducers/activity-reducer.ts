92f6b50af483a1b69f0b6669a20d31fc07afcb53
Jorge Luis Arce <jorgearcedev1@gmail.com>
2025-06-27 22:26:23 -0700

    Almacenando local storage

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -11,8 +11,14 @@ export type ActivityActions =
                 activeId: Activity['id'] 
             }
 
+            const localStorageActivities = () : Activity[] => {
+                const activities = localStorage.getItem('activities');
+                return activities ? JSON.parse(activities) : [];
+            }
+
+
             export const initialState : ActivityState = {
-                activities: [],
+                activities: localStorageActivities(),
                 activeId: ''
 
             }
