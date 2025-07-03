3104aa5b8843fccf19aa3c2b999691c07be1f06c
Jorge Luis Arce <jorgearcedev1@gmail.com>
2025-07-02 19:31:42 -0700

    Mostrando diferencial de calorias

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -15,6 +15,8 @@ export default function CalorieTracker({ activities }: CalorieTrackerProps) {
 
         const caloriesBurned = useMemo (() => activities.reduce((total, activity) => 
         activity.category === 2 ? total + activity.calories : total, 0) , [activities]);
+
+        const netCalories = useMemo(() => caloriesConsumed - caloriesBurned, [activities]);
   return (
     <>
         
@@ -29,6 +31,10 @@ export default function CalorieTracker({ activities }: CalorieTrackerProps) {
             <CalorieDisplay
                 calories={caloriesBurned}
                 text="Ejercicio"
+             />
+             <CalorieDisplay
+                calories={netCalories}
+                text="Diferencia"
              />
         </div>
     </>
